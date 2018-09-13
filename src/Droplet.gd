42d8e3548a2240322ff9ae4978fe4af5909c4526
Kevin Murphy <kmurphy@etsy.com>
2018-09-13 10:44:25 -0700

    better impulse calculation for droplets. also, the blob can jump (poorly)

--- src/Droplet.gd
+++ src/Droplet.gd
@@ -4,12 +4,14 @@ onready var collider:CollisionShape2D = get_node("CollisionShape2D")
 onready var sprite:Sprite = get_node("Sprite");
 export var radius:float = 16 setget set_radius
 export (float, 1, 100, 1) var max_impulse:float = 15
+export var debug_draw:bool = false
 
 var default_radius:float = radius
 var puddle:Node2D = Node2D.new()
 var nav:Navigation2D
 var map:TileMap
 var goal:Vector2
+var goal_path:PoolVector2Array
 
 func _ready():
     collider.shape.radius = radius * 0.4
@@ -22,35 +24,37 @@ func get_nav():
 
 func _physics_process(_delta):
     
+#    update()
     goal = get_goal()
-    
-    update()
     var goal_vec:Vector2 =  (goal - position)
-    var force = goal_vec.length_squared() -  2 * pow(radius, 2) 
+    var puddle_distance = (position - puddle.position).length_squared()
+    var force = max(goal_vec.length_squared(), puddle_distance - pow(radius, 2))
     goal_vec = goal_vec.normalized() 
     goal_vec *= force
-#    var move:Vector2 = get_random_normal()
     var move:Vector2 = goal_vec * puddle.surface_tension
-    move = move.clamped(25)
+#    move = move.clamped(40)
     apply_central_impulse(move)
+    
+func _draw():
+    if not debug_draw:
+        return
+    draw_set_transform_matrix(transform.inverse())
+    if goal and goal != puddle.position:
+        draw_line(position, goal, Color(1, 0, 0))
+    if goal_path:
+        draw_polyline(goal_path, Color(1, 0, 0))
     
 func get_goal() -> Vector2:
     if not puddle:
         goal =  Vector2()
     var goal:Vector2 = puddle.position
-    if (goal - position).length_squared() < pow(4 * radius, 2):
-        return goal 
-    elif nav and nav.has_method('get_simple_path'):
+    if nav and nav.has_method('get_simple_path'):
         var closest:Vector2 = nav.get_closest_point(position)
         var path:PoolVector2Array = nav.get_simple_path(closest, goal)
         if path:
-            goal = puddle.get_tile_center_point(path[1])
+            goal_path = path
+            goal = path[1] # puddle.get_tile_center_point(path[1])
     return goal
-
-#func _draw():
-#    var inv:Transform2D = get_global_transform().inverse()
-#    draw_set_transform_matrix(inv)
-#    draw_circle(goal, 4, Color(0,0,1,1))
 
 func set_radius(r):
     default_radius = r
--- src/Puddle.gd
+++ src/Puddle.gd
@@ -18,6 +18,7 @@ export var gravity:float = 7.0
 export var speed:float = 15.0
 export var max_speed:float = 150.0
 var grace_timer:int = 0
+var grace_exhausted:bool = false
 
 var lv:Vector2 = Vector2()
 var radius:float
@@ -52,12 +53,12 @@ func get_helpers()->void:
         map = get_node(map_path)
     nav = get_node("../Navigation2D")
 
-func _process(_delta)->void:
-    if grace_timer > 0:
-        grace_timer -= 1
 
 func _physics_process(_delta)-> void:
     
+    if grace_timer > 0:
+        grace_timer -= 1
+
     droplets = get_droplets()
     if droplets.size() < droplet_count:
         spawn_droplet()
@@ -76,31 +77,31 @@ func _draw()-> void:
     if not debug_draw:
         return
     draw_circle(Vector2(), radius, Color(1,0,0))
-    var inv:Transform2D = get_global_transform().inverse()
-    draw_set_transform_matrix(inv)
-#    var tile_rect:Rect2 = get_tile_rect(position)
-#    var smol_rect:Rect2 = Rect2(tile_rect.position + Vector2(16,16), tile_rect.size - Vector2(32,32))
-#    draw_rect(tile_rect, Color(1, 1, 1), false)
-#    draw_rect(smol_rect, Color(1,1,1), false)
+    draw_set_transform_matrix(transform.inverse())
     draw_rect(get_blob_rect(), Color(1,0,0), false)
     draw_rect(get_enclosing_rect(), Color(0,1,0), false)
-    draw_polyline($Polygon2D.polygon, Color(1,1,1))
     
 func prepare_brain()-> void:
     brain.add_state(Idle.new(self))
     brain.add_state(Moving.new(self))
     brain.add_state(Climbing.new(self))
     brain.add_state(Grace.new(self))
+    brain.add_state(Jumping.new(self))
     brain.state = 'idle'
     
     brain.add_transition('idle', 'on_move', 'moving')
+    brain.add_transition('idle', 'on_jump', 'jumping')
     
     brain.add_transition('grace', 'on_finished', 'moving')
+    
+    brain.add_transition('jumping', 'on_finished', 'moving')
+    
     
     brain.add_transition('moving', 'on_no_input', 'idle')
     brain.add_transition('moving', 'on_start_climb', 'climbing')
     brain.add_transition('moving', 'on_left_ground', 'grace')
     brain.add_transition('moving', 'on_frozen', 'idle')
+    brain.add_transition('moving', 'on_jump', 'jumping')
     
     brain.add_transition('climbing', 'on_no_input', 'idle')
     brain.add_transition('climbing', 'on_left_wall', 'moving')
@@ -155,7 +156,7 @@ func get_enclosing_rect()-> Rect2:
     var blob_rect:Rect2 = get_blob_rect()
     var blob_size:Vector2 = Vector2(2.0 * radius, 2.0 * radius)
     blob_rect.size += blob_size * 8.0
-    blob_rect.position -= blob_size
+    blob_rect.position -= blob_size * 4.0
     return blob_rect
     
 func get_blob_center()-> Vector2:
@@ -170,15 +171,30 @@ func get_droplets()-> Array:
     return droplets  
     
 func can_move()-> bool:
-    return overlapping_droplets.size() > 2 and (
+    return overlapping_droplets.size() > 0 and (
             is_on_floor() or is_on_wall() or grace_timer)
             
 func can_climb()->bool:
     return((Input.is_action_pressed('ui_left') and left_ray.is_colliding())
              or (Input.is_action_pressed('ui_right') and right_ray.is_colliding()))
 	
+func should_jump()->bool:
+    return(Input.is_action_just_pressed('ui_up') and is_on_floor())
+    
 func get_center_bias_vec()->Vector2:
 	return (position - get_blob_center()) * center_bias
+    
+func get_normal_move()->Vector2:
+        var move:Vector2 = Vector2()
+        var blob_pos:Vector2 = get_blob_center()
+        if Input.is_action_pressed('ui_left'):
+            move -= position - blob_pos
+            move -= Vector2(1, 0) * speed
+        elif Input.is_action_pressed('ui_right'):
+            move -= position - blob_pos
+            move += Vector2(1, 0) * speed
+        return move
+            
 
 func _on_Area2D_body_entered(body)-> void:
     if body.is_in_group('Droplets'):
@@ -202,6 +218,7 @@ class Idle extends State:
     func _physics_process(_delta):
         if target.is_on_floor():
             target.lv -= target.get_center_bias_vec()
+            target.grace_exhausted = false
         target.lv += Vector2(0,1) * target.gravity
         
     func on_move()-> bool:
@@ -210,6 +227,8 @@ class Idle extends State:
                 Input.is_action_pressed('ui_right')
                 ) and target.can_move()
                 
+    func on_jump()->bool:
+        return target.should_jump()
                 
 class MoveState extends State:
     
@@ -226,8 +245,11 @@ class MoveState extends State:
         return not target.can_move()
         
     func on_left_ground():
-        return not (target.grace_timer or target.is_on_floor() or target.is_on_wall())
-    
+        return not (target.grace_exhausted or target.grace_timer or target.is_on_floor() or target.is_on_wall())
+        
+    func on_jump()->bool:
+        return target.should_jump()
+
     
 class Moving extends MoveState:
     
@@ -235,23 +257,10 @@ class Moving extends MoveState:
         name = 'moving'
         
     func _physics_process(_delta) -> void:
-        var move:Vector2 = Vector2()
-        var blob_pos:Vector2 = target.get_blob_center()
-        if Input.is_action_pressed('ui_left'):
-            move -= target.position - blob_pos
-            move -= Vector2(1, 0) * target.speed
-        elif Input.is_action_pressed('ui_right'):
-            move -= target.position - blob_pos
-            move += Vector2(1, 0) * target.speed
-            
-        target.lv += move
+        target.lv += target.get_normal_move()
             
     func on_start_climb():
-        return target.is_on_wall() and (
-            target.left_ray.is_colliding() or
-            target.right_ray.is_colliding()
-            )
-            
+        return target.is_on_wall() and target.can_climb()
    
 class Climbing extends MoveState:
     
@@ -262,7 +271,7 @@ class Climbing extends MoveState:
         var move:Vector2 = Vector2()
         
         if target.can_climb():
-                move += Vector2(0, -1) * target.speed
+            move += Vector2(0, -1) * target.speed
         target.lv += move
                 
     func on_left_wall():
@@ -274,7 +283,33 @@ class Grace extends State:
         name = "grace"
         
     func enter():
-        target.grace_timer = 3
+        target.grace_timer = max(target.grace_timer, 3)
+        target.grace_exhausted = true
         
     func on_finished():
         return true
+        
+class Jumping extends State:
+    var jump_frames:int
+    var orig_max_speed:float
+    
+    func _init(_object).(_object):
+        name = "jumping"
+        
+    func enter():
+        orig_max_speed = target.max_speed
+        target.max_speed = INF
+        jump_frames = 5
+        target.grace_timer = 15
+        
+    func _physics_process(_delta):
+        target.lv += Vector2(0, -100)
+        target.lv += target.get_normal_move()
+        jump_frames -= 1
+
+    func exit():
+        target.max_speed = orig_max_speed
+
+    func on_finished():
+        return jump_frames < 1
+        
